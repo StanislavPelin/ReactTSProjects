eb84aaf279071f59effddaee599e29a78c6ee3a6
Stanislav Pelin <stanislav.pelin@endava.com>
2024-02-19 16:42:50 +0200

    Reviews Project

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,17 @@
 import './App.css';
-import React, {useState} from "react";
-import Question from "./components/Question";
+import React from "react";
+import Review from "./components/Review";
 
 export default function App() {
-    const [activeIndex, setActiveIndex] = useState<number>(-1);
-
     return (
-        <section className="questions">
-            <div className="title">
-                <h2>general questions</h2>
-            </div>
-            <div className="section-center">
-                <Question title="do you accept all major credit cards" isActive={activeIndex === 0}
-                          handleClick={() => {
-                              if (activeIndex !== 0) {
-                                  setActiveIndex(0);
-                              } else {
-                                  setActiveIndex(-1);
-                              }
-                          }}
-                />
-                <Question title="do you support local farmers" isActive={activeIndex === 1}
-                          handleClick={() => {
-                              if (activeIndex !== 1) {
-                                  setActiveIndex(1);
-                              } else {
-                                  setActiveIndex(-1);
-                              }
-                          }}
-                />
-                <Question title="do you use organic ingredients" isActive={activeIndex === 2}
-                          handleClick={() => {
-                              if (activeIndex !== 2) {
-                                  setActiveIndex(2);
-                              } else {
-                                  setActiveIndex(-1);
-                              }
-                          }}
-                />
-            </div>
-        </section>
+        <main>
+            <section className="container">
+                <div className="title">
+                    <h2>our reviews</h2>
+                    <div className="underline"></div>
+                </div>
+                <Review />
+            </section>
+        </main>
     );
 }
